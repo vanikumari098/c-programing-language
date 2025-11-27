2fcb15e00e27dc959c91ddcd15967b3cdcf257b9
vanikumari098 <vanikumari098@gmail.com>
2025-11-27 15:48:33 +0530

    types of veriables.c

---
+++ types of veriables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int age=20;
+    float height=6.7;
+    char grade='B';
+    printf("age:%d\n",age);
+    printf("height:%f\n",height);
+    printf("grade:%c\n",grade);
+    return 0;
+}
