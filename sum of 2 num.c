c3344afad45b470d7148ef6f17683d5a4712ebf1
vanikumari098 <vanikumari098@gmail.com>
2025-11-27 15:21:25 +0530

    sum of 2 num.c

---
+++ sum of 2 num.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include<stdio.h>
+int main()
+{
+ int a,b,sum;
+ printf("enter a value:");
+ scanf("%d",&a);
+ printf("enter b value:");
+ scanf("%d",&b);
+ sum=a+b;
+ printf("%d",sum);
+ return 0;
+ }
