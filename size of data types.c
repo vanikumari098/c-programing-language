a3b792ac87525a16eedaff01e69f0d00c5e80d15
vanikumari098 <vanikumari098@gmail.com>
2025-11-27 16:15:05 +0530

    size of data types.c

---
+++ size of data types.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("the size of int: %d\n",sizeof(int));
+    printf("the size of float: %d\n",sizeof(float));
+    printf("the size of char: %d\n",sizeof(char));
+    printf("the size of double: %d\n",sizeof(double));
+    return 0;
+}
+    
