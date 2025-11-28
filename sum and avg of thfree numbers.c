b00b57ae6c50f782f6222b38471084984287da19
vanikumari098 <vanikumari098@gmail.com>
2025-11-28 15:12:03 +0530

    sum and avg of thfree numbers.c

---
+++ sum and avg of thfree numbers.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+int a,b,c,sum=0;
+float avg=0;
+a=65,b=45,c=68;
+sum=a+b+c; 
+avg=(a+b+c)/3;
+printf("the sum of three numbers:%d\n",sum);
+printf("the avg of three numbers:%f\n",avg);
+}
