20d5d18e35e6fe1b084e309c82d228018e02df92
vanikumari098 <vanikumari098@gmail.com>
2025-12-23 12:48:21 +0530

    Switch.c

---
+++ Switch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main()
+{
+    int day=3;
+    switch(day)
+    {
+        case 1:
+            printf("Monday");
+            break;
+
+        case 2:
+            printf("Tuesday");
+            break;
+
+        case 3:
+            printf("Wednesday");
+            break;
+
+        default:
+            printf("Invalid day");
+    }
+
+    return 0;
+}
