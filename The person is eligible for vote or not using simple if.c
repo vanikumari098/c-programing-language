8694624a44331197c42a6945485e22f269b7ef45
vanikumari098 <vanikumari098@gmail.com>
2025-12-24 12:41:58 +0530

    The person is eligible for vote or not using simple if.c

---
+++ The person is eligible for vote or not using simple if.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int age=0;
+    printf("enter your age:");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("person is eligible for vote");
+    }
+    else
+    {
+    printf("person is not eligible for vote");
+    }
+    return 0;
+}
+
+    
