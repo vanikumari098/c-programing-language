049d9b0efb988330fa687ea2507deb2c0ae99d9f
vanikumari098 <vanikumari098@gmail.com>
2025-12-24 06:56:48 +0530

    Logical operator -NOT.c

---
+++ Logical operator -NOT.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (!(num > 0))
+    {
+        printf("The number is NOT positive");
+    }
+    else
+    {
+        printf("The number is positive");
+    }
+    return 0;
+}
