de67c0cab3ecca99d629f4f00b11b55db6364b29
vanikumari098 <vanikumari098@gmail.com>
2025-12-24 13:26:42 +0530

    Do while loop.c

---
+++ Do while loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    printf("natural from 1to100:\n");
+    do
+    {
+        printf("%4d",i);
+        i++;
+    }
+    while(i<=100);
+    return 0;
+}
