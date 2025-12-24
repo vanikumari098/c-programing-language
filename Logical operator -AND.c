72b4e9c6e2dd8f763a12d647cdaae61f8c9e804f
vanikumari098 <vanikumari098@gmail.com>
2025-12-24 06:46:08 +0530

    Logical operator -AND.c

---
+++ Logical operator -AND.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter any 3 values: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b && a > c)
+    {
+        printf("a is biggest");
+    }
+    else if (b > a && b > c)
+    {
+        printf("b is biggest");
+    }
+    else
+    {
+        printf("c is biggest");
+    }
+
+    return 0;
+}
