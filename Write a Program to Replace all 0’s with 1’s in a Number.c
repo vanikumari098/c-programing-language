f23b9979a411e6adf8bf67a8474557dc2945f29f
vanikumari098 <vanikumari098@gmail.com>
2025-12-29 17:37:01 +0530

    Write a Program to Replace all 0’s with 1’s in a Number.c

---
+++ Write a Program to Replace all 0’s with 1’s in a Number.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    int N = 102301;
+
+    int ans = 0;
+    int i = 0;
+    while (N != 0)
+{
+if (N % 10 == 0)
+            ans = ans + 1 * pow(10, i);
+        else
+            ans = ans + (N % 10) * pow(10, i);
+
+        N = N / 10;
+        i++;
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
